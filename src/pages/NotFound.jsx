6aeb31c55d36f5b0765498abcb81d0c3499a47b3
Rokonze <143441424+Rokonze@users.noreply.github.com>
2024-04-10 12:47:57 +0300

    Complete NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='hero'>
+        <div className="text-center hero-content">
+            <div className="max-w-lg">
+                <h1 className="text-8xl font-bold mb-8">
+                    Oops!
+                </h1>
+                <p className="text-5xl mb-8">Page not found!</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
